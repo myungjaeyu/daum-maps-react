10da0259261da24cb4b290ea9ce2bebfe3f15693
myungjaeyu <rootcode10@gmail.com>
2018-10-18 03:06:03 +0900

    Added DaumMap component.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
+import load from 'load-script'
+
 export default class DaumMap extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
 
-  render() {
-    const {
-      text
-    } = this.props
+    static propTypes = {
+        apiKey : PropTypes.string.isRequired,
+        mapId : PropTypes.string.isRequired,
+        render : PropTypes.func.isRequired
+    }
 
-    return (
-      <div className={styles.test}>
-        {text}
-      </div>
-    )
-  }
+    loadDaumMapScirpt(apiKey) {
+
+        return new Promise(resolve => 
+            window.daum ?
+            resolve({ done : true, daum : window.daum }) 
+            :
+            load(`https://dapi.kakao.com/v2/maps/sdk.js?&autoload=false&appKey=${ apiKey }&libraries=services,clusterer,drawing`, err => 
+                err ? 
+                resolve({ done : false, err : err }) 
+                : 
+                resolve({ done : true, daum : window.daum })))
+    }
+
+    componentDidMount() {
+
+        const { apiKey, mapId, render } = this.props
+
+        this.loadDaumMapScirpt(apiKey)
+            .then(e => 
+                e.done && render(document.getElementById(mapId), daum))
+
+    }
+
+    render() {
+
+        const { mapId } = this.props
+
+        return (<div id={ mapId } className={ styles.daummap }></div>)
+
+    }
+
 }
