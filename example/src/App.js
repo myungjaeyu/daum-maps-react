f5fdde0f69abdcdf9e124b3e126e8aeb322c1f89
myungjaeyu <rootcode10@gmail.com>
2018-10-18 03:06:13 +0900

    Added examples.

--- example/src/App.js
+++ example/src/App.js
@@ -3,11 +3,46 @@ import React, { Component } from 'react'
 import DaumMap from 'daum-maps-react'
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <DaumMap text='Modern React component module' />
-      </div>
-    )
-  }
+
+    customRender = (element, daum) => {
+
+        console.log(element, daum, this)
+
+
+        const lat = 37.503716, lng = 127.044844
+
+        daum.maps.load(_ => {
+
+            const map = new daum.maps.Map(element, {
+                center: new daum.maps.LatLng(lat, lng),
+                level: 3
+            })
+    
+            const marker = new daum.maps.Marker({
+                position: new daum.maps.LatLng(lat, lng)
+            })
+
+            marker.setMap(map)
+
+        })
+
+    }
+
+    render () {
+        return (
+            <div>
+
+                <div style={ { width: '500px', height: '500px' }}>
+
+                    <DaumMap
+                        apiKey='0818f03a98ecd1d911780003319345c2'
+                        mapId={ 'daum-map' }
+                        render= { this.customRender }
+                    />
+
+                </div>
+
+            </div>
+        )
+    }
 }
